acb66f390adab48ffa6714b65d62ee7d252a70af
glowcoil <micahrjohnston@gmail.com>
2021-03-31 22:47:51 -0500

    Path type

--- graphics/src/lib.rs
+++ graphics/src/lib.rs
@@ -1,5 +1,7 @@
 mod geom;
 
+use geom::Vec2;
+
 pub struct Image {
     width: usize,
     height: usize,
@@ -36,5 +38,73 @@ impl<'i> Canvas<'i> {
 
     pub fn push(&'i mut self) -> Canvas<'i> {
         Canvas { target: self.target, scale_factor: self.scale_factor }
+    }
+}
+
+pub struct Path {
+    commands: Vec<PathCmd>,
+    points: Vec<Vec2>,
+}
+
+pub enum PathCmd {
+    Move,
+    Line,
+    Quadratic,
+    Cubic,
+    Close,
+}
+
+pub struct PathBuilder {
+    commands: Vec<PathCmd>,
+    points: Vec<Vec2>,
+}
+
+impl Path {
+    pub fn builder() -> PathBuilder {
+        PathBuilder {
+            commands: Vec::new(),
+            points: Vec::new(),
+        }
+    }
+}
+
+impl PathBuilder {
+    pub fn move_to(&mut self, x: f64, y: f64) -> &mut Self {
+        self.commands.push(PathCmd::Move);
+        self.points.push(Vec2::new(x, y));
+        self
+    }
+
+    pub fn line_to(&mut self, x: f64, y: f64) -> &mut Self {
+        self.commands.push(PathCmd::Line);
+        self.points.push(Vec2::new(x, y));
+        self
+    }
+
+    pub fn quadratic_to(&mut self, x1: f64, y1: f64, x: f64, y: f64) -> &mut Self {
+        self.commands.push(PathCmd::Quadratic);
+        self.points.push(Vec2::new(x1, y1));
+        self.points.push(Vec2::new(x, y));
+        self
+    }
+
+    pub fn cubic_to(&mut self, x1: f64, y1: f64, x2: f64, y2: f64, x: f64, y: f64) -> &mut Self {
+        self.commands.push(PathCmd::Cubic);
+        self.points.push(Vec2::new(x1, y1));
+        self.points.push(Vec2::new(x2, y2));
+        self.points.push(Vec2::new(x, y));
+        self
+    }
+
+    pub fn close(&mut self) -> &mut Self {
+        self.commands.push(PathCmd::Close);
+        self
+    }
+
+    pub fn build(self) -> Path {
+        Path {
+            commands: self.commands,
+            points: self.points,
+        }
     }
 }
