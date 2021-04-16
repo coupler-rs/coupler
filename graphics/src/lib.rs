37dd0702e448e07a80cca4c21f26a8b2c1d04244
glowcoil <micahrjohnston@gmail.com>
2021-04-15 22:10:04 -0500

    path stroking

--- graphics/src/lib.rs
+++ graphics/src/lib.rs
@@ -168,4 +168,121 @@ impl Path {
         }
         path.build()
     }
+
+    pub fn stroke(&self, width: f64) -> Path {
+        #[inline]
+        fn join(path: &mut PathBuilder, width: f64, prev_normal: Vec2, next_normal: Vec2, point: Vec2) {
+            let offset = 1.0 / (1.0 + prev_normal.dot(next_normal));
+            if offset.abs() > 2.0 {
+                path.line_to(point + 0.5 * width * prev_normal);
+                path.line_to(point + 0.5 * width * next_normal);
+            } else {
+                path.line_to(point + 0.5 * width * offset * (prev_normal + next_normal));
+            }
+        }
+
+        #[inline]
+        fn offset(path: &mut PathBuilder, width: f64, contour: &[Vec2], closed: bool, reverse: bool) {
+            let first_point = if closed == reverse {
+                contour[0]
+            } else {
+                *contour.last().unwrap()
+            };
+            let mut prev_point = first_point;
+            let mut prev_normal = Vec2::new(0.0, 0.0);
+            let mut i = 0;
+            loop {
+                let next_point = if i < contour.len() {
+                    if reverse {
+                        contour[contour.len() - i - 1]
+                    } else {
+                        contour[i]
+                    }
+                } else {
+                    first_point
+                };
+
+                if next_point != prev_point || i == contour.len() {
+                    let next_tangent = next_point - prev_point;
+                    let next_normal = Vec2::new(-next_tangent.y, next_tangent.x);
+                    let next_normal_len = next_normal.length();
+                    let next_normal = if next_normal_len == 0.0 {
+                        Vec2::new(0.0, 0.0)
+                    } else {
+                        next_normal * (1.0 / next_normal_len)
+                    };
+
+                    join(path, width, prev_normal, next_normal, prev_point);
+
+                    prev_point = next_point;
+                    prev_normal = next_normal;
+                }
+
+                i += 1;
+                if i > contour.len() {
+                    break;
+                }
+            }
+        }
+
+        let mut path = Path::builder();
+
+        let flattened = self.flatten();
+
+        let mut contour_start = 0;
+        let mut contour_end = 0;
+        let mut closed = false;
+        let mut commands = flattened.commands.iter();
+        loop {
+            let command = commands.next();
+
+            if let Some(PathCmd::Close) = command {
+                closed = true;
+            }
+
+            if let None | Some(PathCmd::Move) | Some(PathCmd::Close) = command {
+                if contour_start != contour_end {
+                    let contour = &flattened.points[contour_start..contour_end];
+
+                    let base = path.commands.len();
+                    offset(&mut path, width, contour, closed, false);
+                    path.commands[base] = PathCmd::Move;
+                    if closed {
+                        path.close();
+                    }
+
+                    let base = path.commands.len();
+                    offset(&mut path, width, contour, closed, true);
+                    if closed {
+                        path.commands[base] = PathCmd::Move;
+                    }
+                    path.close();
+                }
+            }
+
+            if let Some(command) = command {
+                match command {
+                    PathCmd::Move => {
+                        contour_start = contour_end;
+                        contour_end = contour_start + 1;
+                    }
+                    PathCmd::Line => {
+                        contour_end += 1;
+                    }
+                    PathCmd::Close => {
+                        contour_start = contour_end + 1;
+                        contour_end = contour_start;
+                        closed = true;
+                    }
+                    _ => {
+                        unreachable!();
+                    }
+                }
+            } else {
+                break;
+            }
+        }
+
+        path.build()
+    }
 }
