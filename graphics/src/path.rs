645058fcb3830d7c1602c426c3c7f81cefd1d1ca
glowcoil <micahrjohnston@gmail.com>
2021-07-30 22:19:18 -0500

    graphics: approximate circular arcs with cubics

--- graphics/src/path.rs
+++ graphics/src/path.rs
@@ -56,6 +56,47 @@ impl PathBuilder {
         self
     }
 
+    pub fn arc(&mut self, radius: f64, start_angle: f64, end_angle: f64) -> &mut Self {
+        let mut last = self.points.last().cloned().unwrap_or(Vec2::new(0.0, 0.0));
+        let mut vector = Vec2::new(start_angle.cos(), start_angle.sin());
+        let mut angle = 0.0;
+
+        let center = last - radius * vector;
+        let winding = if end_angle < start_angle { -1.0 } else { 1.0 };
+        let total_angle = (end_angle - start_angle).abs();
+
+        // approximate quarter-circle arcs with cubics
+        let quarter_circle = 0.5 * std::f64::consts::PI;
+        let k = (4.0 / 3.0) * (0.25 * quarter_circle).tan();
+        while angle + quarter_circle < total_angle {
+            let tangent = winding * Vec2::new(-vector.y, vector.x);
+
+            let control1 = last + radius * k * tangent;
+            let point = center + radius * tangent;
+            let control2 = point + radius * k * vector;
+            self.cubic_to(control1, control2, point);
+
+            angle += quarter_circle;
+            vector = tangent;
+            last = point;
+        }
+
+        // approximate the remainder of the arc with a single cubic
+        let tangent = winding * Vec2::new(-vector.y, vector.x);
+        let angle_size = total_angle - angle;
+        let k = (4.0 / 3.0) * (0.25 * angle_size).tan();
+
+        let end_vector = Vec2::new(end_angle.cos(), end_angle.sin());
+        let end_tangent = winding * Vec2::new(-end_vector.y, end_vector.x);
+
+        let control1 = last + radius * k * tangent;
+        let point = center + radius * end_vector;
+        let control2 = point - radius * k * end_tangent;
+        self.cubic_to(control1, control2, point);
+
+        self
+    }
+
     pub fn close(&mut self) -> &mut Self {
         self.commands.push(PathCmd::Close);
         self
