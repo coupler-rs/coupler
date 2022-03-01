cb710db11c02afbb2536f0e4edc2f028f32fc76d
Micah Johnston <micah@photophore.systems>
2022-03-01 02:21:02 -0600

    propagate panics upward that occur on worker threads

--- graphics/src/thread_pool.rs
+++ graphics/src/thread_pool.rs
@@ -1,23 +1,26 @@
 use std::marker::PhantomData;
 use std::mem;
-use std::sync::atomic::{AtomicUsize, Ordering};
+use std::panic::{self, AssertUnwindSafe};
+use std::ptr;
+use std::sync::atomic::{AtomicUsize, Ordering, AtomicPtr};
 use std::sync::Arc;
-use std::thread;
-use std::thread::{JoinHandle, Thread};
+use std::thread::{self, JoinHandle, Thread};
+use std::any::Any;
 
 use crossbeam_channel::Sender;
 
 type Task = Box<dyn FnOnce() + Send>;
 
-struct TaskCount {
-    count: AtomicUsize,
+struct Context {
     thread: Thread,
+    task_count: AtomicUsize,
+    panic: AtomicPtr<Box<dyn Any + Send>>,
 }
 
 pub struct ThreadPool {
     handles: Vec<JoinHandle<()>>,
     sender: Option<Sender<Task>>,
-    task_count: Arc<TaskCount>,
+    context: Arc<Context>,
 }
 
 impl ThreadPool {
@@ -26,20 +29,42 @@ impl ThreadPool {
 
         let (sender, receiver) = crossbeam_channel::unbounded::<Task>();
 
-        let task_count =
-            Arc::new(TaskCount { count: AtomicUsize::new(0), thread: thread::current() });
+        let context = Arc::new(Context {
+            thread: thread::current(),
+            task_count: AtomicUsize::new(0),
+            panic: AtomicPtr::new(ptr::null_mut()),
+        });
 
         let mut handles = Vec::with_capacity(num_threads);
         for _ in 0..num_threads {
             let receiver = receiver.clone();
-            let task_count = task_count.clone();
+            let context = context.clone();
 
             let handle = thread::spawn(move || {
                 while let Ok(task) = receiver.recv() {
-                    task();
+                    let result = panic::catch_unwind(AssertUnwindSafe(|| {
+                        task();
+                    }));
 
-                    if task_count.count.fetch_sub(1, Ordering::Release) == 1 {
-                        task_count.thread.unpark();
+                    if context.task_count.fetch_sub(1, Ordering::Release) == 1 {
+                        context.thread.unpark();
+                    }
+
+                    if let Err(err) = result {
+                        unsafe {
+                            let ptr = Box::into_raw(Box::new(err));
+
+                            let result = context.panic.compare_exchange(
+                                ptr::null_mut(),
+                                ptr,
+                                Ordering::Release,
+                                Ordering::Relaxed,
+                            );
+
+                            if result.is_err() {
+                                drop(Box::from_raw(ptr));
+                            }
+                        }
                     }
                 }
             });
@@ -47,17 +72,30 @@ impl ThreadPool {
             handles.push(handle);
         }
 
-        ThreadPool { handles, sender: Some(sender), task_count }
+        ThreadPool { handles, sender: Some(sender), context }
     }
 
     pub fn scope<F>(&mut self, f: F)
     where
         F: FnOnce(&Scope),
     {
-        f(&Scope { pool: self, phantom: PhantomData });
+        let result = panic::catch_unwind(AssertUnwindSafe(|| {
+            f(&Scope { pool: self, phantom: PhantomData });
+        }));
 
-        while self.task_count.count.load(Ordering::Acquire) != 0 {
+        while self.context.task_count.load(Ordering::Acquire) != 0 {
             thread::park();
+        }
+
+        unsafe {
+            let panic = self.context.panic.swap(ptr::null_mut(), Ordering::Relaxed);
+            if !panic.is_null() {
+                panic::resume_unwind(*Box::from_raw(panic));
+            }
+        }
+
+        if let Err(err) = result {
+            panic::resume_unwind(err);
         }
     }
 }
@@ -85,7 +123,7 @@ impl<'p, 's> Scope<'p, 's> {
         let task: Box<dyn FnOnce() + Send> = Box::new(task);
         let task: Box<dyn FnOnce() + Send + 'static> = unsafe { mem::transmute(task) };
 
-        self.pool.task_count.count.fetch_add(1, Ordering::Relaxed);
+        self.pool.context.task_count.fetch_add(1, Ordering::Relaxed);
 
         self.pool.sender.as_ref().unwrap().send(task).unwrap();
     }
