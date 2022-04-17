16d58de43b29662838ea2f3200dd8be23a4e51cf
glowcoil <micahrjohnston@gmail.com>
2022-04-17 17:04:36 -0500

    SplitAtEvents iterator for Buffers

--- plugin/src/buffer.rs
+++ plugin/src/buffer.rs
@@ -1,4 +1,5 @@
 use crate::bus::BusState;
+use crate::process::Event;
 
 use std::marker::PhantomData;
 use std::slice;
@@ -139,6 +140,11 @@ impl<'a, 'b, 'c> Buffers<'a, 'b, 'c> {
     #[inline]
     pub fn chunks(self, chunk_size: usize) -> Chunks<'a, 'b, 'c> {
         Chunks { buffers: self, chunk_size }
+    }
+
+    #[inline]
+    pub fn split_at_events<'e>(self, events: &'e [Event]) -> SplitAtEvents<'a, 'b, 'c, 'e> {
+        SplitAtEvents { buffers: self, events, offset: 0 }
     }
 }
 
@@ -322,5 +328,55 @@ impl<'a, 'b, 'c> Iterator for Chunks<'a, 'b, 'c> {
         self.buffers.len -= chunk_size;
 
         Some(chunk)
+    }
+}
+
+pub struct SplitAtEvents<'a, 'b, 'c, 'e> {
+    buffers: Buffers<'a, 'b, 'c>,
+    events: &'e [Event],
+    offset: usize,
+}
+
+impl<'a, 'b, 'c, 'e> Iterator for SplitAtEvents<'a, 'b, 'c, 'e> {
+    type Item = (Buffers<'a, 'b, 'c>, &'e [Event]);
+
+    #[inline]
+    fn next(&mut self) -> Option<(Buffers<'a, 'b, 'c>, &'e [Event])> {
+        if self.buffers.len == 0 && self.events.is_empty() {
+            return None;
+        }
+
+        let mut event_count = 0;
+        let mut chunk_size = self.buffers.len;
+        for event in self.events {
+            if event.offset > self.offset {
+                chunk_size = (event.offset - self.offset).min(self.buffers.len);
+                break;
+            }
+
+            event_count += 1;
+        }
+
+        let (events, rest) = self.events.split_at(event_count);
+        self.events = rest;
+
+        let chunk = Buffers {
+            offset: self.buffers.offset,
+            len: chunk_size,
+            input_states: self.buffers.input_states,
+            input_indices: self.buffers.input_indices,
+            input_ptrs: self.buffers.input_ptrs,
+            output_states: self.buffers.output_states,
+            output_indices: self.buffers.output_indices,
+            output_ptrs: self.buffers.output_ptrs,
+            phantom: PhantomData,
+        };
+
+        self.buffers.offset += chunk_size;
+        self.buffers.len -= chunk_size;
+
+        self.offset += chunk_size;
+
+        Some((chunk, events))
     }
 }
