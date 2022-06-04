70247897a0a22816e01ee80412f45144e038c60b
glowcoil <micahrjohnston@gmail.com>
2022-06-04 16:39:32 -0500

    cargo-coupler bundle: assemble a list of candidate packages based on --package, --workspace, and --exclude options

--- cargo-coupler/src/main.rs
+++ cargo-coupler/src/main.rs
@@ -1,6 +1,9 @@
 use cargo_metadata::{MetadataCommand};
 use clap::{AppSettings, Args, Parser, Subcommand};
 
+use std::collections::{HashMap, HashSet};
+use std::process;
+
 #[derive(Parser)]
 #[clap(bin_name = "cargo")]
 enum Cargo {
@@ -64,6 +67,65 @@ fn main() {
                 command.manifest_path(manifest_path);
             }
             let metadata = command.exec().unwrap();
+
+            if !cmd.workspace && !cmd.exclude.is_empty() {
+                eprintln!("--exclude can only be used together with --workspace");
+                process::exit(1);
+            }
+
+            let mut packages_by_id = HashMap::new();
+            for (index, package) in metadata.packages.iter().enumerate() {
+                packages_by_id.insert(package.id.clone(), index);
+            }
+
+            // Build a list of candidate packages for bundling
+            let mut candidates = Vec::new();
+            if cmd.workspace {
+                let mut exclude = HashSet::new();
+                for package_name in &cmd.exclude {
+                    exclude.insert(package_name);
+                }
+
+                for package_id in &metadata.workspace_members {
+                    let package_index = packages_by_id[package_id];
+                    if !exclude.contains(&metadata.packages[package_index].name) {
+                        candidates.push(package_index);
+                    }
+                }
+            } else if !cmd.package.is_empty() {
+                // Build an index of packages in the current workspace by name
+                let mut packages_by_name = HashMap::new();
+                for package_id in &metadata.workspace_members {
+                    let package_index = packages_by_id[package_id];
+                    packages_by_name
+                        .insert(metadata.packages[package_index].name.clone(), package_index);
+                }
+
+                for package_name in &cmd.package {
+                    if let Some(&package_index) = packages_by_name.get(package_name) {
+                        candidates.push(package_index);
+                    } else {
+                        eprintln!(
+                            "package `{}` not found in workspace `{}`",
+                            package_name, &metadata.workspace_root
+                        );
+                        process::exit(1);
+                    }
+                }
+            } else if let Some(root) = &metadata.resolve.as_ref().unwrap().root {
+                // If neither --workspace nor --package is specified and there is a
+                // root package, just try to build the root
+                candidates.push(packages_by_id[root]);
+            } else {
+                // If there is no root package, search the entire workspace
+                for package_id in &metadata.workspace_members {
+                    candidates.push(packages_by_id[package_id]);
+                }
+            }
+
+            for &candidate in &candidates {
+                dbg!(&metadata.packages[candidate].id);
+            }
         }
     }
 }
