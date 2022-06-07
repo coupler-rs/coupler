8fa5d5d7bea5b72fdeb36b8d121abd40925342dd
micah <micah@glowcoil.com>
2022-06-07 15:28:44 -0500

    clap: create a static for Factory and create/destroy it in init/deinit.
    The clap_plugin_descriptor needs to be created at runtime based on
    Plugin::info(), and the only place CLAP gives us to clean it up is
    the clap_plugin_factory's deinit. Since init/deinit take no arguments,
    the relevant state needs to be stored in a static, which in turn needs
    to be declared in the clap! entry point macro.

--- src/format/clap.rs
+++ src/format/clap.rs
@@ -1,10 +1,71 @@
 use crate::plugin::*;
 
-use clap_sys::{entry::*, version::*};
+use clap_sys::{entry::*, host::*, plugin::*, plugin_factory::*, version::*};
 
-use std::ffi::c_void;
+use std::ffi::{c_void, CStr};
+use std::marker::PhantomData;
 use std::os::raw::c_char;
+use std::ptr;
 
+#[doc(hidden)]
+pub struct Factory<P> {
+    #[allow(unused)]
+    factory: clap_plugin_factory,
+    descriptor: clap_plugin_descriptor,
+    phantom: PhantomData<P>,
+}
+
+impl<P: Plugin> Factory<P> {
+    fn new() -> Factory<P> {
+        Factory {
+            factory: clap_plugin_factory {
+                get_plugin_count: Self::get_plugin_count,
+                get_plugin_descriptor: Self::get_plugin_descriptor,
+                create_plugin: Self::create_plugin,
+            },
+            descriptor: clap_plugin_descriptor {
+                clap_version: CLAP_VERSION,
+                id: b"\0".as_ptr() as *const c_char,
+                name: b"\0".as_ptr() as *const c_char,
+                vendor: b"\0".as_ptr() as *const c_char,
+                url: b"\0".as_ptr() as *const c_char,
+                manual_url: b"\0".as_ptr() as *const c_char,
+                support_url: b"\0".as_ptr() as *const c_char,
+                version: b"\0".as_ptr() as *const c_char,
+                description: b"\0".as_ptr() as *const c_char,
+                features: [ptr::null()].as_ptr(),
+            },
+            phantom: PhantomData,
+        }
+    }
+
+    unsafe extern "C" fn get_plugin_count(_factory: *const clap_plugin_factory) -> u32 {
+        1
+    }
+
+    unsafe extern "C" fn get_plugin_descriptor(
+        factory: *const clap_plugin_factory,
+        index: u32,
+    ) -> *const clap_plugin_descriptor {
+        let this = &*(factory as *const Self);
+
+        if index == 0 {
+            &this.descriptor
+        } else {
+            ptr::null()
+        }
+    }
+
+    unsafe extern "C" fn create_plugin(
+        _factory: *const clap_plugin_factory,
+        _host: *const clap_host,
+        _plugin_id: *const c_char,
+    ) -> *const clap_plugin {
+        ptr::null()
+    }
+}
+
+#[doc(hidden)]
 #[repr(transparent)]
 pub struct EntryPoint<P> {
     #[allow(unused)]
@@ -13,24 +74,46 @@ pub struct EntryPoint<P> {
 }
 
 impl<P: Plugin> EntryPoint<P> {
-    pub const ENTRY_POINT: EntryPoint<P> = EntryPoint {
-        entry_point: clap_plugin_entry {
-            clap_version: CLAP_VERSION,
-            init: Self::init,
-            deinit: Self::deinit,
-            get_factory: Self::get_factory,
-        },
-        phantom: std::marker::PhantomData,
-    };
+    pub const fn new(
+        init: unsafe extern "C" fn(plugin_path: *const c_char) -> bool,
+        deinit: unsafe extern "C" fn(),
+        get_factory: unsafe extern "C" fn(factory_id: *const c_char) -> *const c_void,
+    ) -> EntryPoint<P> {
+        EntryPoint {
+            entry_point: clap_plugin_entry {
+                clap_version: CLAP_VERSION,
+                init,
+                deinit,
+                get_factory,
+            },
+            phantom: PhantomData,
+        }
+    }
 
-    unsafe extern "C" fn init(_plugin_path: *const c_char) -> bool {
+    pub unsafe extern "C" fn init(
+        _plugin_path: *const c_char,
+        factory: &mut Option<Factory<P>>,
+    ) -> bool {
+        *factory = Some(Factory::new());
+
         true
     }
 
-    unsafe extern "C" fn deinit() {}
+    pub unsafe extern "C" fn deinit(factory: &mut Option<Factory<P>>) {
+        *factory = None;
+    }
 
-    unsafe extern "C" fn get_factory(_factory_id: *const c_char) -> *const c_void {
-        std::ptr::null_mut()
+    pub unsafe extern "C" fn get_factory(
+        factory_id: *const c_char,
+        factory: &Option<Factory<P>>,
+    ) -> *const c_void {
+        if CStr::from_ptr(factory_id) == CStr::from_ptr(CLAP_PLUGIN_FACTORY_ID) {
+            if let Some(factory) = factory {
+                return factory as *const Factory<P> as *const c_void;
+            }
+        }
+
+        ptr::null()
     }
 }
 
@@ -39,7 +122,32 @@ macro_rules! clap {
     ($plugin:ty) => {
         #[allow(non_upper_case_globals)]
         #[no_mangle]
-        static clap_plugin_entry: ::coupler::format::clap::EntryPoint<$plugin> =
-            ::coupler::format::clap::EntryPoint::ENTRY_POINT;
+        static clap_plugin_entry: ::coupler::format::clap::EntryPoint<$plugin> = {
+            // Safety: The CLAP headers specify that init must be called before get_factory or
+            // deinit, init must not be called more than once, and none of the three may be called
+            // after deinit.
+            //
+            // This means that init and deinit can safely form exclusive &mut references to
+            // FACTORY, and that these will not overlap with any & references formed by
+            // get_factory.
+
+            static mut FACTORY: Option<::coupler::format::clap::Factory<$plugin>> = None;
+
+            unsafe extern "C" fn init(plugin_path: *const ::std::os::raw::c_char) -> bool {
+                ::coupler::format::clap::EntryPoint::<$plugin>::init(plugin_path, &mut FACTORY)
+            }
+
+            unsafe extern "C" fn deinit() {
+                ::coupler::format::clap::EntryPoint::<$plugin>::deinit(&mut FACTORY)
+            }
+
+            unsafe extern "C" fn get_factory(
+                factory_id: *const ::std::os::raw::c_char,
+            ) -> *const ::std::ffi::c_void {
+                ::coupler::format::clap::EntryPoint::<$plugin>::get_factory(factory_id, &FACTORY)
+            }
+
+            ::coupler::format::clap::EntryPoint::new(init, deinit, get_factory)
+        };
     };
 }
