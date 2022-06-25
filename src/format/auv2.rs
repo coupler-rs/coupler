8fc3dc3707c2501af78b0cfd5c763903130aae0a
micah <micah@glowcoil.com>
2022-06-25 14:38:53 -0500

    auv2: set up AudioComponentPlugInInterface

--- src/format/auv2.rs
+++ src/format/auv2.rs
@@ -1,9 +1,47 @@
 use std::ffi::c_void;
 use std::ptr;
 
+use auv2_sys::*;
+
+struct Wrapper {
+    #[allow(unused)]
+    interface: AudioComponentPlugInInterface,
+}
+
+#[allow(non_snake_case)]
+impl Wrapper {
+    fn create() -> *mut Wrapper {
+        Box::into_raw(Box::new(Wrapper {
+            interface: AudioComponentPlugInInterface {
+                Open: Some(Self::Open),
+                Close: Some(Self::Close),
+                Lookup: Some(Self::Lookup),
+                reserved: ptr::null_mut(),
+            },
+        }))
+    }
+
+    pub unsafe extern "C" fn Open(
+        _self_: *mut c_void,
+        _mInstance: AudioComponentInstance,
+    ) -> OSStatus {
+        noErr
+    }
+
+    pub unsafe extern "C" fn Close(self_: *mut c_void) -> OSStatus {
+        drop(Box::from_raw(self_ as *mut Wrapper));
+
+        noErr
+    }
+
+    pub unsafe extern "C" fn Lookup(_selector: SInt16) -> AudioComponentMethod {
+        None
+    }
+}
+
 #[doc(hidden)]
 pub unsafe fn auv2_factory(_in_desc: *mut c_void) -> *mut c_void {
-    ptr::null_mut()
+    Wrapper::create() as *mut c_void
 }
 
 #[macro_export]
