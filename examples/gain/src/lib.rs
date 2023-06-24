da6a4e8c9f54394300402e6cefdf189da3dc5014
glowcoil <micahrjohnston@gmail.com>
2023-06-23 23:49:20 -0500

    set up gain example

---
+++ examples/gain/src/lib.rs
@@ -0,0 +1,92 @@
+use std::io::{self, Read, Write};
+
+use coupler::{bus::*, param::*, process::*, *};
+
+pub struct Gain {}
+
+impl Plugin for Gain {
+    type Processor = GainProcessor;
+    type Editor = GainEditor;
+
+    fn info() -> PluginInfo {
+        PluginInfo {
+            name: "Gain".to_string(),
+            vendor: "Vendor".to_string(),
+            url: "https://example.com".to_string(),
+            email: "example@example.com".to_string(),
+            inputs: vec![BusInfo {
+                name: "Input".to_string(),
+            }],
+            outputs: vec![BusInfo {
+                name: "Output".to_string(),
+            }],
+            layouts: vec![
+                Layout {
+                    inputs: vec![Format::Stereo],
+                    outputs: vec![Format::Stereo],
+                },
+                Layout {
+                    inputs: vec![Format::Mono],
+                    outputs: vec![Format::Mono],
+                },
+            ],
+            params: vec![ParamInfo {
+                id: 0,
+                name: "Gain".to_string(),
+                default: 1.0,
+                range: Range::Continuous { min: 0.0, max: 1.0 },
+                display: Box::new(Float),
+            }],
+        }
+    }
+
+    fn create() -> Self {
+        Gain {}
+    }
+
+    fn set_param(&self, id: ParamId, value: ParamValue) {}
+
+    fn get_param(&self, id: ParamId) -> ParamValue {
+        0.0
+    }
+
+    fn save(&self, output: &mut impl Write) -> io::Result<()> {
+        Ok(())
+    }
+
+    fn load(&self, input: &mut impl Read) -> io::Result<()> {
+        Ok(())
+    }
+}
+
+pub struct GainProcessor {}
+
+impl Processor<Gain> for GainProcessor {
+    fn create(plugin: &Gain, config: Config) -> Self {
+        GainProcessor {}
+    }
+
+    fn info(&self) -> ProcessInfo {
+        ProcessInfo { latency: 0 }
+    }
+
+    fn set_param(&mut self, id: ParamId, value: ParamValue) {}
+
+    fn reset(&mut self) {}
+
+    fn process(&mut self, buffers: Buffers, events: Events) {}
+}
+
+pub struct GainEditor {}
+
+impl Editor<Gain> for GainEditor {
+    fn create(plugin: &Gain, context: EditorContext, parent: &ParentWindow) -> Self {
+        GainEditor {}
+    }
+
+    fn size(&self) -> Size {
+        Size {}
+    }
+
+    fn set_param(&mut self, id: ParamId, value: ParamValue) {}
+}
