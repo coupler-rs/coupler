857fc60c79310c26c2002dc5ece966e31f499297
Micah Johnston <micahrjohnston@gmail.com>
2024-01-19 23:22:08 -0600

    mark f32/f64 range impls #[inline]

--- src/params/range.rs
+++ src/params/range.rs
@@ -13,28 +13,34 @@ pub trait DefaultRange: Sized {
 }
 
 impl Range<f32> for std::ops::Range<f32> {
+    #[inline]
     fn steps(&self) -> Option<u32> {
         None
     }
 
+    #[inline]
     fn encode(&self, value: f32) -> ParamValue {
         ((value - self.start) / (self.end - self.start)) as f64
     }
 
+    #[inline]
     fn decode(&self, value: ParamValue) -> f32 {
         (1.0 - value as f32) * self.start + value as f32 * self.end
     }
 }
 
 impl Range<f64> for std::ops::Range<f64> {
+    #[inline]
     fn steps(&self) -> Option<u32> {
         None
     }
 
+    #[inline]
     fn encode(&self, value: f64) -> ParamValue {
         ((value - self.start) / (self.end - self.start)) as f64
     }
 
+    #[inline]
     fn decode(&self, value: ParamValue) -> f64 {
         (1.0 - value as f64) * self.start + value as f64 * self.end
     }
@@ -43,6 +49,7 @@ impl Range<f64> for std::ops::Range<f64> {
 impl DefaultRange for f32 {
     type Range = std::ops::Range<f32>;
 
+    #[inline]
     fn default_range() -> Self::Range {
         0.0..1.0
     }
@@ -51,6 +58,7 @@ impl DefaultRange for f32 {
 impl DefaultRange for f64 {
     type Range = std::ops::Range<f64>;
 
+    #[inline]
     fn default_range() -> Self::Range {
         0.0..1.0
     }
