b1bfeee17cd38b135bfcb1455e325ce6a3b51175
Micah Johnston <micahrjohnston@gmail.com>
2024-01-20 01:15:01 -0600

    implement integer ranges

--- src/params/range.rs
+++ src/params/range.rs
@@ -61,3 +61,64 @@ macro_rules! float_range {
 
 float_range!(f32);
 float_range!(f64);
+
+macro_rules! int_range {
+    ($int:ty) => {
+        impl Range<$int> for std::ops::Range<$int> {
+            #[inline]
+            fn steps(&self) -> Option<u32> {
+                Some(self.end.abs_diff(self.start) as u32)
+            }
+
+            #[inline]
+            fn encode(&self, value: $int) -> ParamValue {
+                let steps_recip = 1.0 / (self.end as f64 - self.start as f64);
+                (value as f64 - self.start as f64 + 0.5) * steps_recip
+            }
+
+            #[inline]
+            fn decode(&self, value: ParamValue) -> $int {
+                let steps = self.end as f64 - self.start as f64;
+                (self.start as f64 + value * steps) as $int
+            }
+        }
+
+        impl Range<$int> for std::ops::RangeInclusive<$int> {
+            #[inline]
+            fn steps(&self) -> Option<u32> {
+                Some(self.end().abs_diff(*self.start()).saturating_add(1) as u32)
+            }
+
+            #[inline]
+            fn encode(&self, value: $int) -> ParamValue {
+                let steps_recip = 1.0 / (*self.end() as f64 + 1.0 - *self.start() as f64);
+                (value as f64 - *self.start() as f64 + 0.5) * steps_recip
+            }
+
+            #[inline]
+            fn decode(&self, value: ParamValue) -> $int {
+                let steps = *self.end() as f64 + 1.0 - *self.start() as f64;
+                (*self.start() as f64 + value * steps) as $int
+            }
+        }
+
+        impl DefaultRange for $int {
+            type Range = std::ops::Range<$int>;
+
+            #[inline]
+            fn default_range() -> Self::Range {
+                0..1
+            }
+        }
+    };
+}
+
+int_range!(u8);
+int_range!(u16);
+int_range!(u32);
+int_range!(u64);
+
+int_range!(i8);
+int_range!(i16);
+int_range!(i32);
+int_range!(i64);
