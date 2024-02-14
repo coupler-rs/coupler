a9f556b6288c675af16f4cddb5f1be697bdf1326
glowcoil <micahrjohnston@gmail.com>
2024-02-14 11:19:16 -0600

    impl BindBuffers for [B; N] where B: BindBuffers

--- src/buffers/bind.rs
+++ src/buffers/bind.rs
@@ -214,3 +214,22 @@ bind_buffers!(B0, B1, B2, B3, B4, B5, B6);
 bind_buffers!(B0, B1, B2, B3, B4, B5, B6, B7);
 bind_buffers!(B0, B1, B2, B3, B4, B5, B6, B7, B8);
 bind_buffers!(B0, B1, B2, B3, B4, B5, B6, B7, B8, B9);
+
+impl<'a, 'b, const N: usize, B> BindBuffers<'a, 'b> for [B; N]
+where
+    B: BindBuffers<'a, 'b>,
+{
+    #[inline]
+    fn bind<I>(buffers: &mut I) -> Result<Self, BindBuffersError>
+    where
+        I: Iterator<Item = AnyBuffer<'a, 'b>>,
+    {
+        let mut results = array::from_fn(|_| None);
+
+        for result in results.iter_mut() {
+            *result = Some(B::bind(buffers)?);
+        }
+
+        Ok(results.map(|result| result.unwrap()))
+    }
+}
