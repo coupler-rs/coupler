6c6cc84f549b7c3f3bf5327c8ddf4bd44aabbaea
glowcoil <micahrjohnston@gmail.com>
2024-02-14 11:13:10 -0600

    impl TryFrom and BindBuffers for arrays of slices

--- src/buffers/bind.rs
+++ src/buffers/bind.rs
@@ -1,3 +1,5 @@
+use std::{array, slice};
+
 use super::{AnyBuffer, Buffer, BufferMut, Buffers};
 
 #[derive(Copy, Clone, Debug)]
@@ -43,6 +45,52 @@ impl<'a, 'b> TryFrom<Buffers<'a, 'b>> for Buffer<'a, 'b> {
     }
 }
 
+impl<'a, 'b, const N: usize> TryFrom<Buffer<'a, 'b>> for [&'b [f32]; N] {
+    type Error = BindBuffersError;
+
+    #[inline]
+    fn try_from(value: Buffer<'a, 'b>) -> Result<Self, Self::Error> {
+        if value.channel_count() != N {
+            return Err(BindBuffersError(()));
+        }
+
+        Ok(array::from_fn(|i| unsafe {
+            slice::from_raw_parts(value.ptrs[i].add(value.offset), value.len)
+        }))
+    }
+}
+
+impl<'a, 'b, const N: usize> TryFrom<AnyBuffer<'a, 'b>> for [&'b [f32]; N] {
+    type Error = BindBuffersError;
+
+    #[inline]
+    fn try_from(value: AnyBuffer<'a, 'b>) -> Result<Self, Self::Error> {
+        Buffer::try_from(value)?.try_into()
+    }
+}
+
+impl<'a, 'b, const N: usize> BindBuffers<'a, 'b> for [&'b [f32]; N] {
+    #[inline]
+    fn bind<I>(buffers: &mut I) -> Result<Self, BindBuffersError>
+    where
+        I: Iterator<Item = AnyBuffer<'a, 'b>>,
+    {
+        match buffers.next() {
+            Some(buffer) => buffer.try_into(),
+            _ => Err(BindBuffersError(())),
+        }
+    }
+}
+
+impl<'a, 'b, const N: usize> TryFrom<Buffers<'a, 'b>> for [&'b [f32]; N] {
+    type Error = BindBuffersError;
+
+    #[inline]
+    fn try_from(value: Buffers<'a, 'b>) -> Result<Self, Self::Error> {
+        value.bind()
+    }
+}
+
 impl<'a, 'b> TryFrom<AnyBuffer<'a, 'b>> for BufferMut<'a, 'b> {
     type Error = BindBuffersError;
 
@@ -69,6 +117,52 @@ impl<'a, 'b> BindBuffers<'a, 'b> for BufferMut<'a, 'b> {
 }
 
 impl<'a, 'b> TryFrom<Buffers<'a, 'b>> for BufferMut<'a, 'b> {
+    type Error = BindBuffersError;
+
+    #[inline]
+    fn try_from(value: Buffers<'a, 'b>) -> Result<Self, Self::Error> {
+        value.bind()
+    }
+}
+
+impl<'a, 'b, const N: usize> TryFrom<BufferMut<'a, 'b>> for [&'b mut [f32]; N] {
+    type Error = BindBuffersError;
+
+    #[inline]
+    fn try_from(value: BufferMut<'a, 'b>) -> Result<Self, Self::Error> {
+        if value.channel_count() != N {
+            return Err(BindBuffersError(()));
+        }
+
+        Ok(array::from_fn(|i| unsafe {
+            slice::from_raw_parts_mut(value.ptrs[i].add(value.offset), value.len)
+        }))
+    }
+}
+
+impl<'a, 'b, const N: usize> TryFrom<AnyBuffer<'a, 'b>> for [&'b mut [f32]; N] {
+    type Error = BindBuffersError;
+
+    #[inline]
+    fn try_from(value: AnyBuffer<'a, 'b>) -> Result<Self, Self::Error> {
+        BufferMut::try_from(value)?.try_into()
+    }
+}
+
+impl<'a, 'b, const N: usize> BindBuffers<'a, 'b> for [&'b mut [f32]; N] {
+    #[inline]
+    fn bind<I>(buffers: &mut I) -> Result<Self, BindBuffersError>
+    where
+        I: Iterator<Item = AnyBuffer<'a, 'b>>,
+    {
+        match buffers.next() {
+            Some(buffer) => buffer.try_into(),
+            _ => Err(BindBuffersError(())),
+        }
+    }
+}
+
+impl<'a, 'b, const N: usize> TryFrom<Buffers<'a, 'b>> for [&'b mut [f32]; N] {
     type Error = BindBuffersError;
 
     #[inline]
