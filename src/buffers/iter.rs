d7da20604c3270e49bf0a4f5d954551e2c8c4c4e
Micah Johnston <micahrjohnston@gmail.com>
2024-02-17 13:44:01 -0600

    in SplitAtEvents, yield all events past the end of the buffer in one go

--- src/buffers/iter.rs
+++ src/buffers/iter.rs
@@ -23,10 +23,21 @@ impl<'a, 'b, 'e> Iterator for SplitAtEvents<'a, 'b, 'e> {
 
     #[inline]
     fn next(&mut self) -> Option<(Buffers<'a, 'b>, Events<'e>)> {
-        if self.buffers.len == 0 && self.events.len() == 0 {
-            return None;
+        if self.buffers.len == 0 {
+            if self.events.len() == 0 {
+                return None;
+            }
+
+            // If we've reached the end of the buffer, yield all remaining events in one go:
+            let buffers = unsafe { Buffers::from_raw_parts(self.buffers.raw, 0) };
+
+            let events = self.events;
+            self.events = Events::new(&[]);
+
+            return Some((buffers, events));
         }
 
+        // Find the first event with a timestamp greater than the current one:
         let mut event_count = 0;
         let mut split = self.buffers.len;
         for event in self.events {
