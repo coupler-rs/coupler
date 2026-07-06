cc746c00f8014f6f8a0cd861607bf7c8d2ab58b6
Micah Johnston <micahrjohnston@gmail.com>
2026-07-06 10:49:26 -0500

    tests for KeyList

--- src/key.rs
+++ src/key.rs
@@ -77,3 +77,74 @@ impl KeyList {
         ids
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+    use std::iter;
+
+    use super::{Key, KeyList};
+
+    struct KeyMap {
+        keys: Vec<String>,
+        list: KeyList,
+    }
+
+    impl KeyMap {
+        fn new() -> KeyMap {
+            KeyMap {
+                keys: Vec::new(),
+                list: KeyList::new(),
+            }
+        }
+
+        pub fn key<'k>(mut self, key: impl Into<Key<'k>>) -> Self {
+            let key = key.into();
+            self.keys.push(key.str.to_string());
+            self.list.key(key);
+
+            self
+        }
+
+        pub fn reserve<'k>(mut self, key: impl Into<Key<'k>>) -> Self {
+            self.list.reserve(key);
+            self
+        }
+
+        fn build(self) -> HashMap<String, u32> {
+            let ids = self.list.into_ids();
+            iter::zip(self.keys, ids).collect()
+        }
+    }
+
+    #[test]
+    fn reorder_keys() {
+        let map1 = KeyMap::new().key("a").key("b").key("c").build();
+        let map2 = KeyMap::new().key("c").key("b").key("a").build();
+
+        // Reordering the list of keys should have no effect on the resulting IDs
+        assert_eq!(map1, map2);
+    }
+
+    #[test]
+    fn add_key() {
+        let map1 = KeyMap::new().key("a").key("b").key("c").build();
+        let map2 = KeyMap::new().key("a").key("b").key("c").key(Key::new(1, "aa")).build();
+
+        // Adding a new key should have no effect on the IDs for keys with lower generation numbers
+        for (key, id) in &map1 {
+            assert_eq!(id, &map2[key]);
+        }
+    }
+
+    #[test]
+    fn remove_key() {
+        let map1 = KeyMap::new().key("a").key("b").key("c").build();
+        let map2 = KeyMap::new().key("a").reserve("b").key("c").build();
+
+        // Removing and reserving a key should have no effect on the IDs for the remaining keys
+        for (key, id) in &map2 {
+            assert_eq!(id, &map1[key]);
+        }
+    }
+}
